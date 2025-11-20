d879b0ca6835e20f5318d0e0406ce3612dab6428
pipocalio <bowserkoopah@gmail.com>
2025-11-19 21:32:06 -0300

    Fix query events

--- KogamaToolsX/Patches/QueryEventListener.cs
+++ KogamaToolsX/Patches/QueryEventListener.cs
@@ -7,11 +7,18 @@ namespace KogamaToolsX.Patches;
 internal static class QueryEventListener
 {
     internal static event Action<MVWorldObjectClient, int> OnQueryEvent;
+    internal static event Action<MVWorldObjectClient> OnQueryEventLocal;
 
     [HarmonyPatch(typeof(WorldNetwork), "CreateQueryEvent")]
     [HarmonyPostfix]
     private static void CreateQueryEvent_Postfix(MVWorldObjectClient root, int instigatorActorNumber)
     {
+        if (!MVGameControllerBase.IsInitialized) 
+            return;
+
         OnQueryEvent?.Invoke(root, instigatorActorNumber);
+
+        if (instigatorActorNumber == MVGameControllerBase.Game.LocalPlayer.ActorNr)
+            OnQueryEventLocal?.Invoke(root);
     }
 }
